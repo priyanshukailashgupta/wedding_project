e31acf6c84151054a8c04013b5e89c1fc023ffd3
priyanshukailashgupta <165934818+priyanshukailashgupta@users.noreply.github.com>
2024-08-05 17:25:06 +0530

    fine

--- satviksha/myapp/views.py
+++ satviksha/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("Hello world , how are u!")
+    return HttpResponse("Hello world , how are u , i am fine!")
